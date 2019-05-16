c4150a55d9fb6356e08edbb0b6cc2e5672c0d634
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-16 03:06:14 -0500

    se codifica el manejo de la peticion o request mediante una funcion constante

--- patient-react-app/src/APIUtilities.js
+++ patient-react-app/src/APIUtilities.js
@@ -1,7 +1,41 @@
-import { ACCESS_TOKEN } from '../constants';
+import { API_BASE_URL, ACCESS_TOKEN } from '../constants';
+
+// funcion para ensamblar el objeto que va a ser enviado por cada peticion a la api
+const request = (options) => {
+    const headers = new Headers({
+        'Content-type': 'application/json',
+    });
+    let token = localStorage.getItem(ACCESS_TOKEN);
+    if(token) {
+        // para mandar el token de autorizacion que se recibio tras haber logueado correctamente
+        headers.append('Autorization', 'Bearer'+token);
+    }
+
+    const defaults = {
+        headers
+    };
+
+    options = Object.assign({}, options);
+
+    // se realiza la peticion
+    return fetch(options.url, options)
+    .then(response => response.json()
+        .then(json => {
+            if(!response.ok){
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+}
 
 export function getCurrentUser() {
     if(!localStorage.getItem(ACCESS_TOKEN)) {
         return Promise.reject("No existe el token de acceso.");
     }
+
+    return request({
+        url: API_BASE_URL + '/paciente/me',
+        method: 'GET' 
+    })
 }
--- patient-react-app/src/constants/index.js
+++ patient-react-app/src/constants/index.js
@@ -1,1 +1,3 @@
+export const API_BASE_URL = 'http://localhost8080/api';
+
 export const ACCESS_TOKEN = 'accessToken';
