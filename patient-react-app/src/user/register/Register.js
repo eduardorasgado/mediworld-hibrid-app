304c9ade0428ca25c96bedf0099db23a669fa851
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-16 19:10:49 -0500

    Register form.
    Register form in process.
    handlerSubmit and isform invalid methods in process.

--- patient-react-app/src/user/register/Register.js
+++ patient-react-app/src/user/register/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 // Importando helpers de api utilities
 import { registerPatient } from '../../APIUtilities';
@@ -37,10 +38,48 @@ export default class Register extends Component {
         }
     }
 
+    /**
+     * Metodo que solicita la funcion register patient de api utilities y pasa los datos
+     * previamente validados para hacer el request a la API
+     */
+    handleSubmit() {
+        notification.success({
+            message: "Mediworld App Registration",
+            description: "Se ha registrado exitosamente"
+        });
+    }
+
+    /**
+     * Metodo que compreba que todos los campos esten validados correctamente previo a activar
+     * el boton de registro
+     */
+    isFormInvalid() {
+        return false;
+    }
+
   render() {
     return (
-      <div>
-        Registrate hoy mismo
+      <div className="register-container">
+        <div className="back-button">
+            <h4>
+                <Link to="/start-over-here">Atrás</Link>
+            </h4>
+        </div>
+        <h1 className="page-title">Crear una cuenta nueva</h1>
+        
+        <div className="register-content">
+            <Form className="register-form" onSubmit={this.handleSubmit}>
+                <FormItem>
+                    <Button type="primary"
+                            htmlType="submit"
+                            size="large"
+                            className="register-form-button"
+                            disabled={this.isFormInvalid()}
+                    >Registrarse</Button>
+                    Ya está registrado? <Link to="/login">Entrar</Link>
+                </FormItem>
+            </Form>
+        </div>
       </div>
     )
   }
