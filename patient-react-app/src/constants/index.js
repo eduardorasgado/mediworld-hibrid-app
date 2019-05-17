c7c814507aa9ca74363d6b96866a3c2a1606561e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-17 03:55:57 -0500

    Constans to register validation.
    new patient attributes max, min, and genre constants

--- patient-react-app/src/constants/index.js
+++ patient-react-app/src/constants/index.js
@@ -1,3 +1,21 @@
 export const API_BASE_URL = 'http://localhost8080/api';
 
 export const ACCESS_TOKEN = 'accessToken';
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 20;
+
+export const EMAIL_MIN_LENGTH = 6;
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 100;
+
+export const NOMBRE_MIN_LENGTH = 2;
+export const NOMBRE_MAX_LENGTH = 40;
+
+export const APELLIDOS_MIN_LENGTH = 2;
+export const APELLIDOS_MAX_LENGTH = 70;
+
+export const GENERO_MASCULINO = 'HOMBRE';
+export const GENERO_FEMENINO = 'MUJER';
