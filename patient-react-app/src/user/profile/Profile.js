64363da50a8cf072cd6f9808be303f503d37d642
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-22 03:20:36 -0500

    Profile component created.
    To load and show patient data

---
+++ patient-react-app/src/user/profile/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div>
+                Estos son los datos del usuario
+            </div>
+        )
+    }
+}
