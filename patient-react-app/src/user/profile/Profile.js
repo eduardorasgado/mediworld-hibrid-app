023dd2fc080388f6c9cb5ab05347d87a4308d0b8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-15 19:37:14 -0500

    new class name in profile component

--- patient-react-app/src/user/profile/Profile.js
+++ patient-react-app/src/user/profile/Profile.js
@@ -26,7 +26,7 @@ export default class Profile extends Component {
     render() {
         return (
             <div className="profile-container">
-                <Row>
+                <Row className="profile-header">
                     <Col span={12}>
                         <h2>
                             Bienvenid@ { this.props.currentUser.nombre } {this.props.currentUser.apellidos}
@@ -60,40 +60,4 @@ export default class Profile extends Component {
 
 const Operations = ({action}) => (
     <Button className="menu-btn" onClick={action}><Icon type="menu" /></Button>
-);
-
-const Description = ({ term, children, span = 12 }) => (
-    <Col span={span}>
-        <div className="description">
-        <div className="term">{term}</div>
-        <div className="detail">{children}</div>
-        </div>
-    </Col>
-);
-  
-const content = (
-<Row>
-    <Description term="Created">Lili Qu</Description>
-    <Description term="Association">
-    <a>421421</a>
-    </Description>
-    <Description term="Creation Time">2017-01-10</Description>
-    <Description term="Effective Time">2017-10-10</Description>
-    <Description term="Remarks" span={24}>
-    Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
-    </Description>
-</Row>
-);
-
-
-  
-const extraContent = (
-<Row>
-    <Col span={12}>
-    <Statistic title="Status" value="Pending" />
-    </Col>
-    <Col span={12}>
-    <Statistic title="Price" prefix="$" value={568.08} />
-    </Col>
-</Row>
 );
