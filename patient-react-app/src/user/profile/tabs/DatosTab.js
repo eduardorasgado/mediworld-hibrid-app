d3cb42c5c6e3ba92e8a8f7ff1b7e100a8f062fe9
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-15 20:51:51 -0500

    including basic data modal component in datos tab component, creating all props importing in datos tab for basic modal componen. Styling form in basic modal.

--- patient-react-app/src/user/profile/tabs/DatosTab.js
+++ patient-react-app/src/user/profile/tabs/DatosTab.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
 import { getAllCountries } from '../../../utils/APIUtilities';
-import { GENERO_FEMENINO, GENERO_MASCULINO, DATE_FORMAT } from '../../../constants';
-import { Modal, Button, Icon, Row, Col, Avatar, Input, Form, Radio, Select, DatePicker } from 'antd';
+import { Button, Icon, Row, Col, Avatar} from 'antd';
 import './DatosTab.css';
-import locale from 'antd/lib/date-picker/locale/es_ES';
 
-// manipulacion de formatos de tiempo
-import moment from 'moment';
+import BasicDataModal from './modals/BasicDataModal';
+
 import 'moment/locale/es';
-const FormItem = Form.Item;
-const RadioGroup = Radio.Group;
-const {Option} = Select;
+
 
 
 export default class DatosTab extends Component {
@@ -34,16 +30,15 @@ export default class DatosTab extends Component {
             // loading de los modales
             basicModalLoading: false,
             availableCountries: []
+            
         }
         this.showBasicModal = this.showBasicModal.bind(this);
         this.handleOkBasicModal = this.handleOkBasicModal.bind(this);
+        this.handleCancelBasicModal = this.handleCancelBasicModal.bind(this);
         this.getAvailalableCountries = this.getAvailalableCountries.bind(this);
         this.handleBasicSubmit = this.handleBasicSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleCountryChange = this.handleCountryChange.bind(this);
-        this.onBlur = this.onBlur.bind(this);
-        this.onFocus = this.onFocus.bind(this);
-        this.onSearch = this.onSearch.bind(this);
         this.handleDateInput = this.handleDateInput.bind(this);
     }
 
@@ -120,18 +115,6 @@ export default class DatosTab extends Component {
         console.log("s");
     }
 
-    onBlur() {
-        console.log('blur');
-      }
-
-    onFocus() {
-        console.log('focus');
-      }
-      
-    onSearch(val) {
-        console.log('search:', val);
-      }
-
     handleDateInput(date, format) {
         let dateVal = ''
         if(date) {
@@ -154,131 +137,29 @@ export default class DatosTab extends Component {
                     </div>
                 </Col>
                 <Col span={14}>
-                <div className="basic-container">
-                    <br></br>   
-                    <Button onClick={() => {this.showBasicModal()}} 
-                        className="btn-data-show">Información básica <Icon type="right" /></Button>
+                    <div className="basic-container">
+                        <br></br>   
+                        <Button onClick={() => {this.showBasicModal()}} 
+                            className="btn-data-show">Información básica <Icon type="right" /></Button>
 
-                    <Button className="btn-data-show">Información personal <Icon type="right" /></Button>
-                    
-                    <Button className="btn-data-show small-btn">Información médica pública <Icon type="right" /></Button>
+                        <Button className="btn-data-show">Información personal <Icon type="right" /></Button>
+                        
+                        <Button className="btn-data-show small-btn">Información médica pública <Icon type="right" /></Button>
 
-                    <Modal
-                        className="modal-visible"
-                        visible={this.state.basicModalVisible}
-                        title="Datos básicos"
-                        onOk={this.handleOkBasicModal}
-                        onCancel={this.handleCancelBasicModal}
-                        footer={[
-                            <Button key="back" onClick={() => {this.handleCancelBasicModal()}}>
-                            Return
-                            </Button>,
-                            <Button key="submit" type="primary" 
-                                loading={this.state.basicModalLoading} 
-                                onClick={this.handleOkBasicModal}>
-                            Submit
-                            </Button>,
-                        ]}
-                        >
-                            <Form className="register-form" 
-                                onSubmit={() => {this.handleBasicSubmit()}}>
-                                <FormItem
-                                >
-                                    <Input
-                                        size="large"
-                                        name="nombre"
-                                        autoComplete="off"
-                                        placeholder="Nombre"
-                                        value={this.state.informacion_basica.nombre}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                    ></Input>
-                                </FormItem>
-                                <FormItem>
-                                    <Input
-                                        size="large"
-                                        name="apellidos"
-                                        autoComplete="off"
-                                        placeholder="Apellidos"
-                                        value={this.state.informacion_basica.apellidos}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                    ></Input>
-                                </FormItem>
-                                <FormItem>
-                                    <Input
-                                        size="large"
-                                        name="estatura"
-                                        autoComplete="off"
-                                        placeholder="Estatura"
-                                        value={this.state.informacion_basica.estatura}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                    ></Input>
-                                </FormItem>
-                                <FormItem>
-                                    <Input
-                                        size="large"
-                                        name="peso"
-                                        autoComplete="off"
-                                        placeholder="Peso"
-                                        value={this.state.informacion_basica.peso}
-                                        onChange={(event) => this.handleInputChange(event)}
-                                    ></Input>
-                                </FormItem>
-                               
-                                <FormItem>
-                                    <RadioGroup name="genero"
-                                                size="large"
-                                                value={this.state.informacion_basica.genero}
-                                                onChange={(event) =>
-                                                    this.handleInputChange(event)
-                                                }
-                                                >
-                                        <Radio value={GENERO_MASCULINO}>Hombre</Radio>
-                                        <Radio value={GENERO_FEMENINO}>Mujer</Radio>
-                                    </RadioGroup>
-                                </FormItem>
-                                <FormItem>
-                                    <label>Fecha de nacimiento</label>
-                                    <DatePicker 
-                                        defaultValue={moment(moment(), DATE_FORMAT)} 
-                                        format={DATE_FORMAT} 
-                                        locale={locale}
-                                        size="large"
-                                        value={moment(this.state.informacion_basica.fechaNacimiento)}
-                                        name="fechaNacimiento"
-                                        onChange={(date, format) =>
-                                            this.handleDateInput(date, format)
-                                        }
-                                    />
-                                </FormItem>
-                                <FormItem>
-                                    <label>País de nacimiento</label>
-                                    <Select
-                                        className="select-country"
-                                        
-                                        placeholder="Selecciona tu país de nacimiento"
-                                        value={this.state.informacion_basica.paisNacimiento}
-                                        showSearch
-                                        name="paisNacimiento"
-                                        
-                                        optionFilterProp="value"
-                                        onSelect={
-                                            this.handleCountryChange
-                                        }
-                                        onFocus={() => {this.onFocus()}}
-                                        onBlur={() => {this.onBlur()}}
-                                        onSearch={() => {this.onSearch()}}
-                                        filterOption={(input, option) =>
-                                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                                        }
-                                    >
-                                        {this.state.availableCountries.map((country) => (
-                                            <Option key={country} value={country}>{country}</Option>
-                                        ))}
-                                    </Select>
-                                </FormItem>
-                            </Form>
-                        </Modal>
-                </div>
+                        <BasicDataModal
+                            currentUser={this.props.currentUser}
+
+                            {...this.state}
+
+                            handleOkBasicModal={this.handleOkBasicModal}
+                            handleCancelBasicModal={this.handleCancelBasicModal}
+                            handleBasicSubmit={this.handleBasicSubmit}
+                            handleInputChange={this.handleInputChange}
+                            handleDateInput={this.handleDateInput}
+                            handleCountryChange={this.handleCountryChange}
+                            
+                        ></BasicDataModal>                    
+                    </div>
                 </Col>
             </Row>
         )
