f2d91a5e328ab3305a2e01bf621d57f1b02e2153
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-16 12:52:27 -0500

    moved server js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
