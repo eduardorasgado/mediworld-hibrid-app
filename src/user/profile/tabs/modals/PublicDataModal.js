8c5c298eee0c46fef4567cc2193d2a8605f09dae
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-18 12:21:40 -0500

    Finished all the switches. onchange in all switches in public data modal.

--- src/user/profile/tabs/modals/PublicDataModal.js
+++ src/user/profile/tabs/modals/PublicDataModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Modal, Button } from 'antd';
+import { Modal, Button, Row, Col, Switch, Form } from 'antd';
+const FormItem = Form.Item;
 
 export default class PublicDataModal extends Component {
     render() {
@@ -21,7 +22,87 @@ export default class PublicDataModal extends Component {
             </Button>,
             ]}
             >
-                this is a shitty modal for public stuff
+                <Form className="register-form" 
+                onSubmit={() => {this.props.handleBasicSubmit()}}>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                Nombre Completo
+                            </Col>
+                            <Col span={6}>
+                                <Switch 
+                                    name="nombre"
+                                    checked={this.props.informacion_medica_publica.nombre}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                Email
+                            </Col>
+                            <Col span={6}>
+                                <Switch
+                                    name="email"
+                                    checked={this.props.informacion_medica_publica.email}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                Teléfono
+                            </Col>
+                            <Col span={6}>
+                                <Switch 
+                                    name="telefono"
+                                    checked={this.props.informacion_medica_publica.telefono}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                Tipo de sangre
+                            </Col>
+                            <Col span={6}>
+                                <Switch 
+                                    name="tipo_sangre"
+                                    checked={this.props.informacion_medica_publica.tipo_sangre}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                País
+                            </Col>
+                            <Col span={6}>
+                                <Switch
+                                    name="pais"
+                                    checked={this.props.informacion_medica_publica.pais}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={18}>
+                                Alergias
+                            </Col>
+                            <Col span={6}>
+                                <Switch
+                                    name="alergias"
+                                    checked={this.props.informacion_medica_publica.alergias}
+                                    onChange={(checked, event) => {this.props.handlePublicInputChange(checked, event)}} />
+                            </Col>
+                        </Row>
+                    </FormItem>
+                </Form>
             </Modal>
         )
     }
