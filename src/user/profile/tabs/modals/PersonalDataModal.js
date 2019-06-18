737844edfbe1d1726858ca282b825f5a8bf15807
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-18 11:11:42 -0500

    last 2 inputs and 2 checkboxes in personal info.

--- src/user/profile/tabs/modals/PersonalDataModal.js
+++ src/user/profile/tabs/modals/PersonalDataModal.js
@@ -84,6 +84,72 @@ export default class PersonalDataModal extends Component {
                             </Col>
                         </Row>                        
                     </FormItem>
+                    <FormItem>
+                        <Input
+                            size="large"
+                            name="num_seguro_social"
+                            autoComplete="off"
+                            placeholder="Numero de seguro social"
+                            value={this.props.informacion_personal.num_seguro_social}
+                            onChange={(event) => this.props.handlePersonalInputChange(event)}
+                        ></Input>
+                    </FormItem>
+                    <FormItem>
+                        <Input
+                            size="large"
+                            name="seguro_medico"
+                            autoComplete="off"
+                            placeholder="Seguro Médico"
+                            value={this.props.informacion_personal.seguro_medico}
+                            onChange={(event) => this.props.handlePersonalInputChange(event)}
+                        ></Input>
+                    </FormItem>
+                    <FormItem>
+                        <Row>
+                            <Col span={12}>
+                                <Checkbox 
+                                    onChange={(event) => {this.props.onPersonalCheckboxChecked(event)}}
+                                    name="internado_hospitalario"
+                                    value={this.props.informacion_personal.internado_hospitalario}>
+                                        <div className="question-checkbox">
+                                            ¿Alguna vez has estado internado?
+                                        </div>
+                                </Checkbox>
+                            </Col>
+                            <Col span={12}>
+                                <Checkbox 
+                                    onChange={(event) => {this.props.onPersonalCheckboxChecked(event)}}
+                                    name="cirugia"
+                                    value={this.props.informacion_personal.cirugia}>
+                                        <div className="question-checkbox">
+                                            ¿Hás tenido alguna cirugía?
+                                        </div>
+                                </Checkbox>
+                            </Col>
+                        </Row>  
+                        <Row>
+                            <Col span={12}>
+                                <Checkbox 
+                                    onChange={(event) => {this.props.onPersonalCheckboxChecked(event)}}
+                                    name="actividad_fisica"
+                                    value={this.props.informacion_personal.actividad_fisica}>
+                                        <div className="question-checkbox">
+                                            ¿Realiza actividad física?
+                                        </div>
+                                </Checkbox>
+                            </Col>
+                            <Col span={12}>
+                                <Checkbox 
+                                    onChange={(event) => {this.props.onPersonalCheckboxChecked(event)}}
+                                    name="antecedentes_enfermedades_importantes"
+                                    value={this.props.informacion_personal.antecedentes_enfermedades_importantes}>
+                                        <div className="question-checkbox">
+                                            ¿Antecedentes de enfermedades importantes?
+                                        </div>
+                                </Checkbox>
+                            </Col>
+                        </Row>                         
+                    </FormItem>
                 </Form>
             </Modal>
         )
